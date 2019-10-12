d8ebccb22c8b77c484c3bbb0cb7674d28f16575f
Laura Palumbo <lallap8817@gmail.com>
2019-10-12 20:04:25 +0200

    implement bubble sort

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-$a = 1;
+function cmp( $a, $b ) { 
+  if(  $a->weight ==  $b->weight ){ return 0 ; } 
+  return ($a->weight < $b->weight) ? -1 : 1;
+} 
+
+usort($unsortedObjectArray,'cmp');
 
 ?>
